bfd83df42401d222b2035afb95b1969bad3e915a
Azeez-coding <meetmusaonline03@gmail.com>
2024-12-29 17:49:25 +0100

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,6 @@
+myset1={1,1,2,22,2}
+myset2={1,1,4,4,4,2,3}
+
+print(myset1.diffrence(myset2))
+
+print(myset1.symmetric_diffrence(myset2))
---
+++ activity1.py
@@ -0,0 +1,19 @@
+
+my_tuple = (1,2,3)
+print(my_tuple)
+
+my_tuple = (3,"hello",3.4)
+print(my_tuple)
+
+my_tuple = ("nose", [8,4.3],(3,3,2))
+print(my_tuple)
+
+my_tuple = ('p','i','k','a','c','h','u')
+
+print(my_tuple[0])
+print(my_tuple[5])
+
+print('sliced:',my_tuple)
+
+for letter in my_tuple:
+    print("letter")
